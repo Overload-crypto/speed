35661ac84548cb1934a093652c4ba3ead678a6bc
Overload-crypto <mychalbrian13@gmail.com>
2025-04-28 18:58:12 +0300

    Update settings.py

--- speed/settings.py
+++ speed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m3oxkjmzbffsa=@40jl81%n-v&cl0!g8fzvaw&a+@s23%p&q19
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost']
 
 
 # Application definition
